b85dcb903c448d10f597a5c3130c20237a4516f2
Alex Jamshidi <alexjamshidi@Alexs-MacBook-Air.local>
2026-05-14 17:40:16 +0100

    code fully functional

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//You can edit ALL of the code here
 const searchInput = document.getElementById("search-input");
 const searchCount = document.getElementById("search-count");
 const episodeSelector = document.getElementById("episode-selector");
@@ -6,16 +5,15 @@ let allEpisodes = [];
 
 function setup() {
   allEpisodes = getAllEpisodes();
-  createDropdownOptions(allEpisodes);
+  populateEpisodeSelect(allEpisodes);
   makePageForEpisodes(allEpisodes);
-  showAllEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-
+  document.getElementById("episode-select").innerHTML = "";
   document.getElementById("episodes").innerHTML = "";
+  populateEpisodeSelect(episodeList);
+  showAllEpisodes(episodeList);
 }
 
 function showAllEpisodes(allEpisodes) {
@@ -46,6 +44,16 @@ function createEpisodeCode(episode) {
   return `S${seasonNum.padStart(2, 0)}E${episodeNum.padStart(2, 0)}`;
 }
 
+function populateEpisodeSelect(episodes) {
+  const select = document.getElementById("episode-select");
+  episodes.forEach((episode) => {
+    const option = document.createElement("option");
+    option.value = episode.id;
+    option.textContent = `${createEpisodeCode(episode)} - ${episode.name}`;
+    select.appendChild(option);
+  });
+}
+
 searchInput.addEventListener("input", function () {
   const searchTerm = searchInput.value.toLowerCase();
 
@@ -61,12 +69,16 @@ searchInput.addEventListener("input", function () {
   searchCount.textContent = `Displaying ${filteredEpisodes.length} / ${allEpisodes.length} episodes`;
 });
 
+const episodeSelect = document.getElementById("episode-select");
+episodeSelect.addEventListener("change", (event) => {
+  const targetId = `ep-${event.target.value}`;
+
+  if (targetId) {
+    const element = document.getElementById(targetId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  }
+});
+
 window.onload = setup;
-
-/*
-
-
-   4. The medium-sized image for the episode
-   5. The summary text of the episode
-
-  */
